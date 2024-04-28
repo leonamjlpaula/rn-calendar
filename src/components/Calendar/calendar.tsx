7682ab5ad4b6023b58ec6908a7b087e61e393f29
Leonam de Paula <leonam.depaula@shipveho.com>
2024-04-28 10:55:08 -0500

    Moved styles to end of file

--- src/components/Calendar/calendar.tsx
+++ src/components/Calendar/calendar.tsx
@@ -5,71 +5,6 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { generateCalendar } from "../../utils/generators";
 import { addMonths, subMonths, format, isSameMonth, isEqual } from "date-fns";
 
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: 288,
-    borderColor: "#B7B7B7",
-    borderWidth: 1,
-    borderRadius: 7,
-    backgroundColor: "#131313",
-  },
-  monthControlsContainer: {
-    flexDirection: "row",
-    gap: 9,
-    position: "absolute",
-    right: 21,
-    top: 20,
-  },
-  titleContainer: {
-    flexDirection: "row",
-    paddingLeft: 18,
-    paddingTop: 8,
-    gap: 4,
-    alignItems: "baseline",
-  },
-  month: {
-    fontFamily: "Poppins_500Medium",
-    fontSize: 15,
-  },
-  year: {
-    fontFamily: "Poppins_600SemiBold",
-    fontSize: 25,
-  },
-  daysContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    rowGap: 8,
-    columnGap: 32,
-    marginTop: 24,
-    paddingLeft: 11,
-    paddingRight: 13,
-  },
-  dayStyle: {
-    height: 27,
-    width: 27,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  selectedDayStyle: {
-    backgroundColor: "#00A19B",
-    height: 27,
-    width: 27,
-    borderRadius: 100,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dayText: {
-    fontFamily: "Poppins_500Medium",
-    fontSize: 15,
-  },
-  dayTextNotInMonth: {
-    fontFamily: "Poppins_500Medium",
-    fontSize: 15,
-    color: "rgba(255, 255, 255, 0.2)",
-  },
-});
-
 export const Calendar = () => {
   const [componentWidth, setComponentWidth] = useState(0);
   const [daysInCalendar, setDaysInCalendar] = useState<Date[]>([]);
@@ -144,3 +79,68 @@ export const Calendar = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 288,
+    borderColor: "#B7B7B7",
+    borderWidth: 1,
+    borderRadius: 7,
+    backgroundColor: "#131313",
+  },
+  monthControlsContainer: {
+    flexDirection: "row",
+    gap: 9,
+    position: "absolute",
+    right: 21,
+    top: 20,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    paddingLeft: 18,
+    paddingTop: 8,
+    gap: 4,
+    alignItems: "baseline",
+  },
+  month: {
+    fontFamily: "Poppins_500Medium",
+    fontSize: 15,
+  },
+  year: {
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 25,
+  },
+  daysContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    rowGap: 8,
+    columnGap: 32,
+    marginTop: 24,
+    paddingLeft: 11,
+    paddingRight: 13,
+  },
+  dayStyle: {
+    height: 27,
+    width: 27,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  selectedDayStyle: {
+    backgroundColor: "#00A19B",
+    height: 27,
+    width: 27,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dayText: {
+    fontFamily: "Poppins_500Medium",
+    fontSize: 15,
+  },
+  dayTextNotInMonth: {
+    fontFamily: "Poppins_500Medium",
+    fontSize: 15,
+    color: "rgba(255, 255, 255, 0.2)",
+  },
+});
